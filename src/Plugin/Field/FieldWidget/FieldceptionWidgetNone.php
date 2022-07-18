217efe919686cce41e1002e67088573304e06e0e
JaceRider <jacerider@gmail.com>
2022-07-18 13:09:50 -0500

    Better migration.

---
+++ src/Plugin/Field/FieldWidget/FieldceptionWidgetNone.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\fieldception\Plugin\Field\FieldWidget;
+
+/**
+ * Plugin implementation of the 'fieldception table' widget.
+ *
+ * @FieldWidget(
+ *   id = "fieldception",
+ *   label = @Translation("Deprecated"),
+ *   field_types = {"fieldception"},
+ *   no_ui = TRUE
+ * )
+ */
+class FieldceptionWidgetNone extends FieldceptionWidgetBase {
+
+}
